5ff89037c4ccb1cb805d731deaf9141b00d2f265
shaaahidkn <shahidkhanmultimedia@gmail.com>
2026-07-13 23:32:07 +0530

    2 projects of with async JS

---
+++ script.js
@@ -0,0 +1,37 @@
+
+let name = "shahid";
+let greeting = "hi, my name is "
+
+let myGreeting = greeting + "" + name ;
+console.log(myGreeting);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// => Variables (var, let, const), Data Types, Operators
+// => Conditional Statements (if, else, switch, ternary operator)
+// => Looping Statements (for, while, do...while, for...in, for…of)
+// => Objects, Arrays, Functions
+// => Array Methods (map, filter, reduce, forEach, find, etc.)
+// => String Methods
+// => Number Methods
+// => Set / Map
+// => Spread and Rest Operators
+// => Type Conversions
+// => Hoisting
+// => Closures
+// => Exception Handling: Learn and practice try, catch, finally, and throw
+
+
